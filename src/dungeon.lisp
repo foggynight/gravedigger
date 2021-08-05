dc0789cf181b90f183d461c5e73547a8dfb1c771
foggynight <foggynight709@gmail.com>
2021-08-05 00:57:27 -0230

    Move GENERATE-DUNGEON parameter list onto its...
    ...own line, wrap lines of LOOP forms in SET-DUNGEON-REGION-TILE-SYMBOL,
    and fix PRINT-DUNGEON docstring.

--- src/dungeon.lisp
+++ src/dungeon.lisp
@@ -9,10 +9,11 @@
   "Dungeon structure representing a dungeon in the world."
   (tiles #2A() :type simple-array))
 
-(defun generate-dungeon (&key
-                           (height *default-dungeon-height*)
-                           (width *default-dungeon-width*)
-                           (symbol *default-tile-symbol*))
+(defun generate-dungeon
+    (&key
+       (height *default-dungeon-height*)
+       (width *default-dungeon-width*)
+       (symbol *default-tile-symbol*))
   "Generate a new dungeon."
   (make-dungeon :tiles (make-tile-array2 height width symbol)))
 
@@ -26,12 +27,15 @@
 
 (defun set-dungeon-region-tile-symbol (dungeon region symbol)
   "Set the tile symbol of the tiles within the given region of a dungeon."
-  (loop for y from (region-top-left-y region) to (region-bottom-right-y region)
-        do (loop for x from (region-top-left-x region) to (region-bottom-right-x region)
-                 do (setf (tile-symbol (aref (dungeon-tiles dungeon) y x)) symbol))))
+  (loop for y from (region-top-left-y region)
+          to (region-bottom-right-y region)
+        do (loop for x from (region-top-left-x region)
+                   to (region-bottom-right-x region)
+                 do (setf (tile-symbol (aref (dungeon-tiles dungeon) y x))
+                          symbol))))
 
 (defun print-dungeon (dungeon &optional (stream *standard-output*))
-  "Print the text representation of a dungeon STREAM."
+  "Print the text representation of a dungeon to STREAM."
   (let ((tiles (dungeon-tiles dungeon)))
     (fresh-line stream)
     (dotimes (y (array-dimension tiles 0))
